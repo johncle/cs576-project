663c5dcd55f88e73c3b67d83a02ea769f0db87fd
Nathan G <njgrab135@gmail.com>
2024-12-15 17:28:13 -0500

    Added Start Menu Scene
    - Added start menu with "how to play" button and explanation panel
    - Touched up look of UI

---
+++ Assets/StartMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    public GameObject howToPlayPanel;
+
+    public void HowToPlayOpen(){
+        howToPlayPanel.SetActive(true);
+    }
+
+    public void HowToPlayClose(){
+        howToPlayPanel.SetActive(false);
+    }
+
+    public void OnStart(){
+        SceneManager.LoadScene("Level");
+    }
+}
