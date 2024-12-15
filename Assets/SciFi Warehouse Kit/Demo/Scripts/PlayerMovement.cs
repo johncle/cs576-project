2f54723fad86df7653c311155a7a9dc2cb765d0e
ccargill05 <cargill0523@gmail.com>
2024-12-14 21:09:01 -0500

    Keycard inventory management with door animation

--- Assets/SciFi Warehouse Kit/Demo/Scripts/PlayerMovement.cs
+++ Assets/SciFi Warehouse Kit/Demo/Scripts/PlayerMovement.cs
@@ -15,21 +15,21 @@ public class PlayerMovement : MonoBehaviour
     Vector3 velocity;
     bool isGrounded;
 
-    
-     public AudioClip footStepSound;
-     public float footStepDelay;
- 
-     private float nextFootstep = 0;
+
+    public AudioClip footStepSound;
+    public float footStepDelay;
+
+    private float nextFootstep = 0;
 
     // Update is called once per frame
     void Update()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        if (isGrounded && velocity.y <0)
-            {
+        if (isGrounded && velocity.y < 0)
+        {
             velocity.y = -2f;
-            }
+        }
 
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
@@ -37,7 +37,7 @@ public class PlayerMovement : MonoBehaviour
         Vector3 motion = transform.right * x + transform.forward * z;
         controller.Move(motion * speed * Time.deltaTime);
 
-        if(Input.GetButtonDown("Jump") && isGrounded)
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
@@ -45,16 +45,16 @@ public class PlayerMovement : MonoBehaviour
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
 
-         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W) && isGrounded)
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W) && isGrounded)
+        {
+            nextFootstep -= Time.deltaTime;
+            if (nextFootstep <= 0)
             {
-             nextFootstep -= Time.deltaTime;
-             if (nextFootstep <= 0) 
-                {
-                 GetComponent<AudioSource>().PlayOneShot(footStepSound, 0.7f);
-                 nextFootstep += footStepDelay;
-                }
-             }
-         }
+                GetComponent<AudioSource>().PlayOneShot(footStepSound, 0.7f);
+                nextFootstep += footStepDelay;
+            }
+        }
+    }
 }
 
 
